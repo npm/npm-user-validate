95605307ad5033e81870d94c64f04cfb822d42c5
Benjamin Coe <ben@npmjs.com>
2016-10-20 13:53:51 -0700

    fix: added regex for blocking illegal characters in usernames

--- test/email.test.js
+++ test/email.test.js
@@ -2,25 +2,25 @@ var test = require('tap').test
 var v = require('../npm-user-validate.js').email
 
 test('email misses an @', function (t) {
-  err = v('namedomain')
+  var err = v('namedomain')
   t.type(err, 'object')
   t.end()
 })
 
 test('email misses a dot', function (t) {
-  err = v('name@domain')
+  var err = v('name@domain')
   t.type(err, 'object')
   t.end()
 })
 
 test('email misses a string before the @', function (t) {
-  err = v('@domain')
+  var err = v('@domain')
   t.type(err, 'object')
   t.end()
 })
 
 test('email is ok', function (t) {
-  err = v('name@domain.com')
+  var err = v('name@domain.com')
   t.type(err, 'null')
   t.end()
 })
--- test/pw.test.js
+++ test/pw.test.js
@@ -2,31 +2,31 @@ var test = require('tap').test
 var v = require('../npm-user-validate.js').pw
 
 test('pw contains a \'', function (t) {
-  err = v('\'')
+  var err = v('\'')
   t.type(err, 'null')
   t.end()
 })
 
 test('pw contains a :', function (t) {
-  err = v(':')
+  var err = v(':')
   t.type(err, 'null')
   t.end()
 })
 
 test('pw contains a @', function (t) {
-  err = v('@')
+  var err = v('@')
   t.notOk(err, 'null')
   t.end()
 })
 
 test('pw contains a "', function (t) {
-  err = v('"')
+  var err = v('"')
   t.type(err, 'null')
   t.end()
 })
 
 test('pw is ok', function (t) {
-  err = v('duck')
+  var err = v('duck')
   t.type(err, 'null')
   t.end()
 })
